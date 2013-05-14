fdfad4e59199032e9335f0d6f8b3546f076772c1
Wendell Smith <wendell.smith@yale.edu>
2013-05-14 10:14:17 -0400

    fixed vecrand3d

---
+++ vecrand3d.hpp
@@ -0,0 +1,66 @@
+#ifndef VECRAND_H
+#define VECRAND_H
+#define VEC3D
+
+#include <iostream>
+#include <ctime>
+
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/random/normal_distribution.hpp>
+
+#include "vec.hpp"
+
+typedef Vector<double> Vec;
+typedef Numvector<double, 2> Pair;
+typedef Nvector<Vec, 2> VecPair;
+using namespace std;
+
+typedef boost::mt19937 engine;
+typedef boost::normal_distribution<> distribution;
+typedef boost::variate_generator<engine&, distribution > generator;
+
+Vec rand3d();
+
+void seed(unsigned int n);
+void seed();
+
+class gaussVec {
+    protected:
+        engine e;
+        distribution distro;
+        generator gauss;
+    public:
+        gaussVec(double sigma) : distro(0,sigma), gauss(e,distro){};
+        void set(double sigma){distro = distribution(0,sigma);};
+        Vec generate(){return Vec(gauss(),gauss(),gauss());};
+        void seed(unsigned int n){e.seed(n);};
+        void seed(){e.seed(static_cast<unsigned int>(time(0)));};
+};
+
+class bivariateGauss {
+    protected:
+        engine e;
+        distribution distro;
+        generator gauss;
+        double x11;
+        double x21;
+        double x22;
+        
+    public:
+        bivariateGauss(const double s1=1, const double s2=1, const double corr=0)
+                : distro(0,1), gauss(e,distro){set(s1, s2, corr);};
+        void set(const double s1, const double s2, const double corr);
+        Pair generate();
+        Vec genVec(){return Vec(gauss(), gauss(), gauss());};
+        VecPair genVecs();
+        void seed(unsigned int n){e.seed(n);};
+        void seed(){e.seed(static_cast<unsigned int>(time(0)));};
+};
+
+//~ int main(){
+    //~ randengine.seed(static_cast<unsigned int>(time(0)));
+    //~ for(int i=0; i<10; i++) cout << rand3d() << endl;
+//~ }
+
+#endif
