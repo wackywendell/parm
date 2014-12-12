1cab54aef3992b03561cd94cf63f540d8644a5c7
Wendell Smith <wendellwsmith@gmail.com>
2014-12-12 10:16:38 -0500

    Improved Packing class

--- pyparm/jammed.py
+++ pyparm/jammed.py
@@ -5,13 +5,21 @@ from math import sqrt
 
 def rand_sphere(d0):
     """
-    Get random points within a sphere. Returns array of shape (d0, 3).
+    Get random points within a sphere of radius 1. Returns array of shape (d0, 3).
     """
     p1 = np.random.randn(d0, 3)
     m = np.sqrt(np.sum(p1**2, axis=1))
     
     rad = pow(np.random.rand(d0), 1.0/3.0)
     return (p1.T * (rad/m)).T
+
+def rand_disk(d0):
+    """
+    Get random points within a disk of radius 1. Returns array of shape (d0, 2).
+    """
+    phi = np.random.rand(d0)*(2*np.pi)
+    rad = pow(np.random.rand(d0), 1.0/2.0)
+    return (np.array([cos(phi), sin(phi)]) * rad).T
 
 def DM(rs, d):
     """Dynamical matrix for array rs, size ds. Assumes epsilon is the
@@ -253,6 +261,7 @@ class Packing:
         self.rs = np.array(rs) / float(L)
         self.sigmas = np.array(sigmas) / float(L)
         self.gamma = gamma
+        self.L = L
         
         self.N = len(self.sigmas)
         n, self.ndim = np.shape(self.rs)
@@ -285,7 +294,7 @@ class Packing:
         sigmadists = np.add.outer(self.sigmas, self.sigmas)/2.
         dists = np.sqrt(np.sum(diffs**2, axis=0))
         
-        return dists - sigmadists < tol, diffs
+        return dists - sigmadists < tol, diffs*self.L
     
     def backbone(self, tol=1e-8):
         """Returns (backbone indices, neighbor matrix)"""
@@ -345,7 +354,208 @@ class Packing:
     def dist(self, other, tol=1e-8, maxt = 1000000):
         tree = self.dist_tree(other, tol=tol)
         tree.expand(maxt)
-        return sqrt(tree.curbest().distsq)
+        return sqrt(tree.curbest().distsq)*self.L
+        
+    @staticmethod
+    def _cage_pts(xyz, neighbor_xyzs, sigma, neighbor_sigmas, L, M, R):
+        """Finds points within a distance R of point xyz that do not conflict with neigbors"""
+        pts = rand_sphere(M)*R + xyz
+        for nxyz, nsig in zip(neighbor_xyzs, neighbor_sigmas):
+            dpts = np.remainder(pts - nxyz + L/2.0, L) - L/2.0
+            dists_sq = np.sum(dpts**2, axis=1)
+            goodix = dists_sq >= ((nsig + sigma)/2.0)**2
+            pts = pts[goodix, :]
+        return pts
+
+    def cages(self, M=10000, R=None, Rfactor = 1.2, padding=0.1, Mfactor=0.1):
+        """
+        Find all cages in the current "packing". 
+        
+        The algorithm uses Monte Carlo: it finds M random points within a sphere of radius R from
+        each particle, and sees if that particle could sit there without conflicting with other particles.
+        Then (number of accepted points) / (number of test points) * (volume of sphere) is the
+        volume of the cage.
+        
+        The algorithm is adaptive: if not enough test points are accepted (n < M * Mfactor), it tries
+        more test points. If any test points are within `padding` of the edge, `R` is (temporarily)
+        expanded.
+        
+        Parameters
+        ----------
+        M : Number of points in the sphere to test
+        R : Size of sphere to test (will be expanded if necessary)
+        Rfactor : How much to increase R by when the cage doesn't fit
+        padding : How much larger the sphere should be than the cage (if it isn't, the sphere is 
+                    expanded)
+        Mfactor : Mfactor * M is the minimum number of points to find per cage. If they aren't 
+                    found, more points are tested.
+        
+        Returns
+        -------
+        points : a list of (A x 3) lists, A indeterminate (but larger than M * Mfactor), with each 
+                    list corresponding to the points within one cage.
+        Vs : The approximate volumes of each cage.
+        """
+        if R is None: R = min(self.sigmas) * 0.2
+        neighbordict = {}
+        
+        psets = []
+        Vs = []
+        for n, (xyz, s) in enumerate(zip(self.rs, self.sigmas)):
+            curR = R
+            curpow = -1
+            nxyzs = nsigs = None
+            
+            def get_pts():
+                pts = self._cage_pts(xyz, nxyzs, s, nsigs, 1.0, M, curR)
+                maxdist = np.max(np.sqrt(np.sum((pts-xyz)**2, axis=1))) if len(pts) > 0 else 0
+                return pts, maxdist
+            
+            pts, maxdist = [], curR
+            while maxdist * (1. + padding) > curR:
+                # print(n, curpow, maxdist * (1. + padding), curR)
+                curpow += 1
+                curR = R * pow(Rfactor, curpow)
+                curM = M
+                if curpow not in neighbordict:
+                    pack = Packing(self.rs, self.sigmas + curR)
+                    cur_neighbors, _ = pack.neighbors(tol=0)
+                    cur_neighbors[np.diag_indices_from(cur_neighbors)] = False
+                    neighbordict[curpow] = cur_neighbors
+                cur_neighbors = neighbordict[curpow]
+                nix = cur_neighbors[n]
+                nxyzs = self.rs[nix, :]
+                nsigs = self.sigmas[nix]
+                pts, maxdist = get_pts()
+                if maxdist * (1. + padding) > curR: continue
+            
+                while len(pts) < Mfactor * M:
+                    # print(curM, len(pts), Mfactor * M, len(pts) < Mfactor * M)
+                    pts2, maxdist2 = get_pts()
+                    maxdist = max((maxdist, maxdist2))
+                    pts = np.concatenate((pts, pts2))
+                    curM += M
+                    if maxdist * (1. + padding) > curR: break
+                if maxdist > 0.5:
+                    raise ValueError('Cage size filling entire space, cannot continue.')
+            
+            fracgood = len(pts) / curM
+            r = fracgood**(1./3.) * curR
+            V = fracgood * 4 * np.pi / 3 * (curR**3)
+            psets.append(pts)
+            Vs.append(V)
+        return psets**self.L, np.array(Vs)*(self.L**self.ndim)
+    
+    def scene(pack, cmap=None, rot=0, camera_height = 0.7,  camera_dist = 1.5, angle=None, 
+            lightstrength = 1.1, orthographic=False, pad=None, floatercolor=(.6,.6,.6)):
+        """
+        Render a scene.
+        
+        Requires `vapory` package, which requires the `povray` binary.
+        
+        Parameters
+        ----------
+        cmap : a colormap 
+        
+        Returns
+        -------
+        scene : vapory.Scene, which can be rendered using its `.render()` method.
+        """
+        import vapory
+        import numpy as np
+        
+        try:
+            import matplotlib as mpl
+            import matplotlib.cm as mcm
+            vmin, vmax = min(pack.sigmas), max(pack.sigmas)
+            sm = mcm.ScalarMappable(norm=mpl.colors.Normalize(vmin, vmax), cmap=cmap)
+            cols = [sm.to_rgba(s) for s in pack.sigmas]
+        except ImportError:
+            if not isinstance(cmap, list):
+                raise ValueError("matplotlib could not be imported, and cmap not recognizeable as a list")
+            cols = list(cmap)
+        except TypeError:
+            if not isinstance(cmap, list):
+                raise ValueError("matplotlib could not convert cmap to a colormap, and cmap not recognizeable as a list")
+            cols = list(cmap)
+
+        if floatercolor is not None:
+            ix, _ = pack.backbone()
+            ns, = np.nonzero(~ix)
+            for n in ns: cols[n] = floatercolor
+        rs = np.remainder(pack.rs+.5, 1)-.5
+        spheres = [
+            vapory.Sphere(xyz, s/2., vapory.Texture( vapory.Pigment( 'color', col[:3] )))
+            for xyz, s, col in zip(rs, pack.sigmas, cols)
+        ]
+
+        extent = (-.5, .5)
+        corners = [np.array((x,y,z)) for x in extent for y in extent for z in extent]
+        pairs = [(c1, c2) for c1 in corners for c2 in corners if np.allclose(np.sum((c1-c2)**2), 1) and sum(c1-c2) > 0]
+
+        radius = 0.01
+        col = vapory.Texture( vapory.Pigment( 'color', [.5,.5,.5]))
+        cyls = [vapory.Cylinder(c1, c2, 0.01, col) for c1,c2 in pairs]
+        caps = [vapory.Sphere(c, radius, col) for c in corners]
+        
+        
+        light_locs = [
+            [ 8.,  5., -3.],
+            [-6.,  6., -5.],
+            [-6., -7., -4.],
+            [ 10., -5., 7.]
+        ]
+        rotlocs = [[x*np.cos(rot) - z*np.sin(rot),y, z*np.cos(rot) + x*np.sin(rot)] for x,y,z in light_locs]
+        lights = [
+            #vapory.LightSource( [2,3,5], 'color', [1,1,1] ),
+            vapory.LightSource(loc, 'color', [lightstrength]*3 ) for loc in rotlocs
+        ]
+        cloc = [np.cos(rot)*camera_dist,camera_dist*camera_height,np.sin(rot)*camera_dist]
+        mag = sqrt(sum([d**2 for d in cloc]))
+        direction = [-v *2/ mag for v in cloc]
+        
+        if angle is None:
+            if pad is None: pad = max(pack.sigmas)
+            w = sqrt(2) + pad
+            angle = float(np.arctan2(w, 2*camera_dist))*2*180/np.pi
+        camera = vapory.Camera('location', cloc, 'look_at', [0,0,0], 'angle', angle)
+        # vapory.Camera('orthographic', 'location', cloc, 'direction', direction, 'up', [0,2,0], 'right', [2,0,0])
+         
+        return vapory.Scene( camera, objects= lights + spheres + cyls + caps + [vapory.Background( "color", [1,1,1] )])
+    
+    def DM(self):
+        """Dynamical matrix for array rs, size ds. Assumes epsilon is the
+        same for all.
+        """
+        N = len(self.sigmas)
+        rs = self.rs
+        d = self.ndim
+        M=np.zeros((d*N,d*N))
+        
+        for i in range(N):
+            sigi = self.sigmas[i]
+            for j in range(i):
+                rijvec=rs[i,:]-rs[j,:]
+                rijvec=rijvec-np.around(rijvec)
+                rijsq=np.sum(rijvec**2)
+                dij=(sigi+self.sigmas[j])/2
+                dijsq=dij**2
+                if rijsq < dijsq:
+                    rij=np.sqrt(rijsq)
+                    rijouter = np.outer(rijvec,rijvec)
+                    Mij1=-rijouter/rijsq/dijsq
+                    Mij2=(1-rij/dij)*(rijouter/rijsq-np.eye(2))/rij/dij
+                    Mij=Mij1+Mij2
+                    
+                    M[d*i:d*i+d,d*j:d*j+d]=Mij
+                    M[d*j:d*j+d,d*i:d*i+d]=Mij
+                    M[d*i:d*i+d,d*i:d*i+d]-=Mij
+                    M[d*j:d*j+d,d*j:d*j+d]-=Mij
+        return M / self.L**2
+    
+    def DM_freqs(self):
+        ew, ev = np.linalg.eig(self.DM())
+        return np.sqrt(np.abs(ew)) / (np.pi)
 
 class Packing3d:
     def __init__(self, rs, sigmas, gamma = 0.0, L=1.0):
@@ -393,7 +603,8 @@ class Packing3d:
     @staticmethod
     def _cage_pts(xyz, neighbor_xyzs, sigma, neighbor_sigmas, L, M, R):
         """Finds points within a distance R of point xyz that do not conflict with neigbors"""
-        pts = rand_sphere(M)*R + xyz
+        rpoints = rand_sphere(M) if self.ndim == 3 else rand_disk(M)
+        pts = rpoints*R + xyz
         for nxyz, nsig in zip(neighbor_xyzs, neighbor_sigmas):
             dpts = np.remainder(pts - nxyz + L/2.0, L) - L/2.0
             dists_sq = np.sum(dpts**2, axis=1)
