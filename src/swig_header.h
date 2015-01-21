335740319c5682d425136fcd6e73f3f5041a9032
Wendell Smith <wendellwsmith@gmail.com>
2015-01-21 17:26:09 -0500

    Added pragmas to ignore warnings from sim_wrap*.cxx

---
+++ src/swig_header.h
@@ -0,0 +1,9 @@
+// **************** BEGIN PREPARED HEADER **************** //
+// Some basic GCC initialization stuff to go at the beginning of the SWIG generated C++
+
+#ifdef __GNUG__
+// Ignore these two directives *only* in sim_wrap.cxx
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#pragma GCC diagnostic ignored "-Wconversion"
+#endif
+// ****************  END PREPARED HEADER  **************** //
