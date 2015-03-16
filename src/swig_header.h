e4539cc092f711da0df0d52bea9a069dae0f8e40
Wendell Smith <wendellwsmith@gmail.com>
2015-03-16 17:48:43 -0400

    Better detection of clang++

--- src/swig_header.h
+++ src/swig_header.h
@@ -1,9 +1,13 @@
 // **************** BEGIN PREPARED HEADER **************** //
 // Some basic GCC initialization stuff to go at the beginning of the SWIG generated C++
 
-#ifdef __GNUG__
+#if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__))
+// || defined(__INTEL_COMPILER)
+
 // Ignore these two directives *only* in sim_wrap.cxx
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#pragma GCC diagnostic ignored "-Wconversion"
 #endif
+
+#pragma GCC diagnostic ignored "-Wconversion"
+
 // ****************  END PREPARED HEADER  **************** //
