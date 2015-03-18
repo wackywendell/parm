336f0466de0c7cfebbb4fdf13df24a1d0af615f2
Wendell Smith <wendellwsmith@gmail.com>
2015-03-18 14:47:06 -0400

    Fewer pressures necessary in example packer

--- src/bin/packer.cpp
+++ src/bin/packer.cpp
@@ -65,12 +65,13 @@ int main(){
     // Its called "Hertzian" for historical reasons
     // It takes a pointer to the box, a pointer to the atoms, and a "skin depth" for the NeighborList
     boost::shared_ptr<NListed<HertzianAtom, HertzianPair> > 
+    boost::shared_ptr<NListed<HertzianAtom, HertzianPair> >
         hertzian(new NListed<HertzianAtom, HertzianPair>(obox, atomptr, 0.1*sigma));
     boost::shared_ptr<NeighborList> nl = hertzian->nlist();
     // ^ this is the Interaction
     
     // Note that NListed is a class template; its an Interaction that
-    // takes various structs as template parameters, and turns them into 
+    // takes various structs as template parameters, and turns them into
     // a neighbor Interaction
     // See interaction.hpp for a whole bunch of them
     // Also note that the NeighborList is not the same as the
@@ -109,13 +110,13 @@ int main(){
     writefile(atoms, *obox);
     
     //Print out total energy, kinetic energy, and potential energy
-    cout << "H: " << collec.Hamiltonian() << " K: " << collec.kinetic() 
+    cout << "H: " << collec.Hamiltonian() << " K: " << collec.kinetic()
         << " U: " << hertzian->energy(*obox) << " phi: " << (Vs/obox->V()) << "\n";
     
     // Run the simulation! And every _ steps, write a frame to the .xyz
     // file and print out the energies again
     uint i = 0;
-    for(flt curP=startP; curP>P0; curP/=2){
+    for(flt curP=startP; curP>P0; curP/=10){
         cout << "P: " << curP << "\n";
         collec.setP(curP);
         while (true) {
@@ -160,7 +161,7 @@ void writefile(AtomVec& atoms, OriginBox& obox){
     // Line N+4: [Comment line, whatever you want, left blank here]
     // Line N+5: [element type, here C for carbon]\t[x]\t[y]\t[z]
     // ...
-    // And so on. each set of N atoms/coordinates corresponds to a "frame", 
+    // And so on. each set of N atoms/coordinates corresponds to a "frame",
     // which then make a movie. There must be the same N in each frame for VMD.
     // Note that if this is compiled as a 2D simulation, it will leave out
     // the z coordinate, and VMD can't handle that.
