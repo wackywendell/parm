wackywendell/parm

commit b591db4d6c5885c698131808fcd20ef1b4de90f9

No longer matches "interaction.hpp" etc.

src/namereplacer.py
@@ -11,7 +11,7 @@ for f in (list(Path('src').glob('*.*pp')) + list(Path('src/bin').glob('*.*pp')) 
     txt1 = txt = f.open('r').read()
 
     for old, new in replacements:
-        txt = re.sub(r'\b{}\b'.format(old), new, txt)
+        txt = re.sub(r'\b\binteraction\b(?![.][hc]pp)\b'.format(old), new, txt)
 
     #newp = f.with_suffix(f.suffix + '.new')
     #print(newp)

commit d6dc1d27fbbf56178c40102ca50411dee02aaa5a

Fixed namereplacer.py, it broke after commit b591db4d6c5885c698131808fcd20ef1b4de90f9

src/namereplacer.py
@@ -35,7 +35,7 @@ for f in fs:
     txt1 = txt = f.open('r').read()
 
     for old, new in replacements:
-        txt = re.sub(r'\b\binteraction\b(?![.][hc]pp)\b'.format(old), new, txt)
+        txt = re.sub(r'\b\b{}\b(?![.][hc]pp)\b'.format(old), new, txt)
 
     #newp = f.with_suffix(f.suffix + '.new')
     #print(newp)